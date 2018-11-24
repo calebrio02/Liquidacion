ef3c48d393af4648daf910a8ea292fc35bafc018
juanc <juanc@WINDOWS-7H1OU6U>
2018-11-23 23:34:57 -0600

    Creacion de la tabla de liquidacion con sus respectivos metodos

---
+++ CalculoPlanilla/src/modelo/Liquidacion.java
@@ -0,0 +1,20 @@
+package modelo;
+
+public class Liquidacion {
+
+
+ public void ControlDeLiquidacion() {
+	
+	
+	
+}
+
+public void TresMeses() {}
+
+public void Renuncia() {}
+
+public void DespidoConResponsabilidad() {}
+
+public void DespidoSinResponsabilidad() {}
+
+}
