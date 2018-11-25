76f14d3f908fdef01c61fcc3875a392c47194c02
juanc <juanc@WINDOWS-7H1OU6U>
2018-11-25 12:12:07 -0600

    atributos que hacian falta fueron agregados, avance en la GUI

--- CalculoPlanilla/src/modelo/Empleado.java
+++ CalculoPlanilla/src/modelo/Empleado.java
@@ -4,8 +4,60 @@ import javax.swing.JOptionPane;
 public abstract class Empleado extends Persona{
 	
 	String nombreTrabajo;
+	String dni;
 	String lugarTrabajo;
+	String fechaInicio;
+	String fechaSalida;
+	String tiempoLaborado; // Acomodar dela siguiente forma --> #dias, #Meses, #Anios
+	String motivoSalida;
+	String tipoPago;
+	double vacaciones;
+	double aguinaldo;
+	double preaviso;
+	double salarioBruto;
+	double cesantia;
+	double saldoTotal;
+
+	int diasVacaciones;
 	
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getFechaInicio() {
+		return fechaInicio;
+	}
+	public void setFechaInicio(String fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+	public String getFechaSalida() {
+		return fechaSalida;
+	}
+	public void setFechaSalida(String fechaSalida) {
+		this.fechaSalida = fechaSalida;
+	}
+	public double getSalarioBruto() {
+		return salarioBruto;
+	}
+	public void setSalarioBruto(double salarioBruto) {
+		this.salarioBruto = salarioBruto;
+	}
+	
+	
+public int getDiasVacaciones() {
+		return diasVacaciones;
+	}
+	public void setDiasVacaciones(int diasVacaciones) {
+		this.diasVacaciones = diasVacaciones;
+	}
+	public String getTiempoLaborado() {
+		return tiempoLaborado;
+	}
+	public void setTiempoLaborado(String tiempoLaborado) {
+		this.tiempoLaborado = tiempoLaborado;
+	}
 public Empleado() {
 }
 public String getNombreTrabajo() {
@@ -20,12 +72,15 @@ public String getLugarTrabajo() {
 public void setLugarTrabajo(String lugarTrabajo) {
 	this.lugarTrabajo = lugarTrabajo;
 }
-public void ingresarEmpleado() {
+public void ingresarEmpleado() {  // ignorar esto, ya que se debe enlazar con los ingresa de la interfaz
 	
-	ingresaPersona();
-	setNombreTrabajo(JOptionPane.showInputDialog("Digite el nombre de la empresa para la que labora "+getNombre()));
-	setLugarTrabajo(JOptionPane.showInputDialog("Ingrese el puesto que desempena "+getNombre()));
+	//ingresaPersona();
+	//setNombreTrabajo(JOptionPane.showInputDialog("Digite el nombre de la empresa para la que labora "+getNombre()));
+//	setLugarTrabajo(JOptionPane.showInputDialog("Ingrese el puesto que desempena "+getNombre()));
 }
+
+
+
 public String muestraEmpleado() {
 	
 	String respuesta="";
